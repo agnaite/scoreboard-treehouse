89a2457617d695b6523f0feb5386fb9f6faeb90c
Agnė Klimaitė <agnaite@me.com>
2016-12-19 16:37:09 -0800

    Added props

--- src/index.js
+++ src/index.js
@@ -2,15 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-function Application() {
+function Application(props) {
     return (
-        <div className="application">
+        <div className="scoreboard">
             <div className="header">
-                <h1>Scoreboard</h1>
+                <h1>{ props.title }</h1>
+            </div>
+            <div className="players">
+                <div className="player">
+                    <div className="player-name">Agne Klimaite</div>
+                    <div className="player-score">
+                        <div className="counter">
+                            <button className="counter-action decrement">-</button>
+                            <div className="counter-score">31</div>
+                            <button className="counter-action increment">+</button>
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
     );
 }
 
-ReactDOM.render(<Application />, 
+ReactDOM.render(<Application title="My Scoreboard" />, 
                 document.getElementById('container'));
