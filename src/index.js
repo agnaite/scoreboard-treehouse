5fdd1811d009c4a899d12ea5ae191b6d4fe03a76
Agnė Klimaitė <agnaite@me.com>
2016-12-28 18:04:44 -0800

    added ability to add and remove players to scoreboard

--- src/index.js
+++ src/index.js
@@ -16,12 +16,35 @@ var PLAYERS = [
     }
 ];
 
+var nextId = 4;
+
 var AddPlayer = React.createClass({
+
+    propTypes: {
+        onAdd: React.PropTypes.func.isRequired,
+    },
+
+    getInitialState: function() {
+        return {
+            name: "",
+        };
+    },
+
+    onSubmit: function(e) {
+        e.preventDefault();
+        this.props.onAdd(this.state.name);
+        this.setState({name: ""});
+    },
+
+    onNameChange: function(e) {
+        this.setState({name: e.target.value});
+    },
+
     render: function() {
         return (
             <div className="add-player-form">
-                <form>
-                    <input type="text" />
+                <form onSubmit={this.onSubmit}>
+                    <input type="text" value={this.state.name} onChange={this.onNameChange} />
                     <input type="submit" value="Add Player" />
                 </form>
             </div>
@@ -91,6 +114,7 @@ function Player(props) {
     return (
         <div className="player">
             <div className="player-name">
+                <a className="remove-player" onClick={props.onRemove}>x</a>
                 {props.name}
             </div>
             <div className="player-score">
@@ -104,6 +128,7 @@ Player.propTypes = {
     name: React.PropTypes.string.isRequired,
     score: React.PropTypes.number.isRequired,
     onScoreChange: React.PropTypes.func.isRequired,
+    onRemove: React.PropTypes.func.isRequired,
 };
 
 
@@ -131,6 +156,20 @@ var Application = React.createClass({
         this.setState(this.state);
     },
 
+    onPlayerAdd: function(name)  {
+        this.state.players.push({name: name,
+                                 score: 0,
+                                 id: nextId});
+        this.setState(this.state);
+        nextId += 1;
+    },
+
+    onRemovePlayer: function(index) {
+        this.state.players.splice(index, 1);
+        this.setState(this.state);
+        console.log(index);
+    },
+
     render: function() {
         return (
             <div className="scoreboard">
@@ -141,6 +180,7 @@ var Application = React.createClass({
                         return (
                             <Player 
                                 onScoreChange={function(delta) { this.onScoreChange(index, delta)}.bind(this) }
+                                onRemove={function() { this.onRemovePlayer(index)}.bind(this) }
                                 name={player.name} 
                                 score={player.score} 
                                 key={player.id} />
@@ -148,7 +188,7 @@ var Application = React.createClass({
                     }.bind(this))}
                 </div>
 
-                <AddPlayer />
+                <AddPlayer onAdd={this.onPlayerAdd} />
             </div>
         );
     }
